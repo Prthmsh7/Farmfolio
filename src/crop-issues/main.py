2ec53ee3de85da435a20fe01a4b7359e5470033f
00Manas-Singh00 <110744893+00Manas-Singh00@users.noreply.github.com>
2024-05-02 21:06:56 +0530

    changes crop issues folder

--- src/crop-issues/main.py
+++ src/crop-issues/main.py
@@ -1,27 +1,60 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+# from flask_wtf import FlaskForm
+# from wtforms import FileField, SubmitField
+# from werkzeug.utils import secure_filename
+# import os
+# from wtforms.validators import InputRequired
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'supersecretkey'
+# app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+# class UploadFileForm(FlaskForm):
+#     file = FileField("File", validators=[InputRequired()])
+#     submit = SubmitField("Upload File")
+
+# @app.route('/', methods=['GET',"POST"])
+# @app.route('/home', methods=['GET',"POST"])
+# def home():
+#     form = UploadFileForm()
+#     if form.validate_on_submit():
+#         file = form.file.data # First grab the file
+#         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+#         return "File has been uploaded."
+#     return render_template('templates/index.html', form=form)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
-from werkzeug.utils import secure_filename
-import os
-from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['SECRET_KEY'] = 'supersecretkey'  # Change this to your secret key
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
 
-class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
-def home():
-    form = UploadFileForm()
-    if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded."
-    return render_template('index.html', form=form)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+class UploadForm(FlaskForm):
+    photo = FileField('Image', validators=[
+        FileRequired(),
+        FileAllowed(photos, 'Images only!')
+    ])
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('templates/index.html', form=form)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return f'<img src="{url_for("static", filename="static/uploads/" + filename)}">'
